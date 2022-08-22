1e316e8907160e837da655ad82537606747677b5
LuisSerafini <serafinilf@gmail.com>
2022-08-22 13:48:14 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Bem Vindo Programação Web Tarde</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+        if(10>12){
+        echo "Meu nome";
+        echo "<script>";
+        echo "alert('Teste')";
+        echo "</script>";
+        }else{
+            echo "Condição Falsa !";
+        }
+
+        ?>
+
+        <h1>Bem Vindo Programação Web Tarde</h1><br/>
+        <img src="./imagens/baixados.jpg">
+
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script>
+            document.write("<br/>"+"Olá alunos !");
+        </script>
+    </body>
+</html>
