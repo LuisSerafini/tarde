45aa822a989c15b9d6d5c1d2d8edd1a1357e6636
LuisSerafini <serafinilf@gmail.com>
2022-09-19 13:37:51 -0300

    projeto novo

---
+++ clientes.php
@@ -0,0 +1,8 @@
+<?php include "cabecalho.php";?>
+
+
+<h1>Clientes</h1>
+
+
+
+<?php include "rodape.php"; ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "cabecalho.php";?>
+
+
+<h1>Página Inicial</h1>
+
+
+
+<?php include "rodape.php"; ?>
